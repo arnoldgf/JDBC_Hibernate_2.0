88a28dee581c879181a4b765b19a7965ace9717d
arnoldgf <arnoldgf@mail.ru>
2024-12-20 15:14:45 +0300

    1.1

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,9 +1,17 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+import java.security.Provider;
 import java.sql.*;
 
 public class Util {
-
+    //jdbc
     private static final String URL = "jdbc:mysql://localhost:3306/javadatabase";
     public static final String USER = "root";
     public static final String PASS = "1qaz@WSX";
@@ -13,6 +21,42 @@ public class Util {
             return DriverManager.getConnection(URL, USER, PASS);
         } catch (SQLException e) {
             throw new RuntimeException("Ошибка подключения к базе данных: " + e.getMessage(), e);
+        }
+    }
+
+    //hibernate
+    private static SessionFactory sessionFactory;
+
+    static {
+        try {
+            Configuration configuration = new Configuration()
+                    .setProperty("hibernate.connection.driver_class", "com.mysql.cj.jdbc.Driver")
+                    .setProperty("hibernate.connection.url", "jdbc:mysql://localhost:3306/javadatabase")
+                    .setProperty("hibernate.connection.username", "root")
+                    .setProperty("hibernate.connection.password", "1qaz@WSX")
+                    .setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect")
+                    .setProperty("hibernate.show_sql", "true")
+                    .setProperty("hibernate.hbm2ddl.auto", "update");
+
+            configuration.addAnnotatedClass(User.class);
+
+            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                    .applySettings(configuration.getProperties())
+                    .build();
+
+            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static Session getSession() {
+        return sessionFactory.openSession();
+    }
+
+    public static void closeSession(Session session) {
+        if (session != null && sessionFactory.isOpen()) {
+            session.close();
         }
     }
 }
